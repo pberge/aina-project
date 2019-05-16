f49ca74d42e199476d95a5a60231a6e4e2bba234
pberge <pereberge@gmail.com>
2019-05-16 12:06:39 +0200

    refactor

---
+++ backend/Aina/Services/PricesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aina.Services
+{
+    public class PricesService
+    {
+        public int GetPrices()
+        {
+            return 230;
+        }
+    }
+}
