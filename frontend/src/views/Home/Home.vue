bb5fc48c78d8ded305ee1403a2f8559d94845dcf
pberge <pereberge@gmail.com>
2019-06-04 12:26:46 +0200

    identation

--- frontend/src/views/Home/Home.vue
+++ frontend/src/views/Home/Home.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="home">
-      <img class="photo" src="@/assets/home.jpg"/>
-      <div class="title-container row">
-				<span class="title">{{$t("home.title")}}</span>
-      </div>
-  </div>
+	<div class="home">
+		<img class="photo" src="@/assets/home.jpg"/>
+		<div class="title-container row">
+			<span class="title">{{$t("home.title")}}</span>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Home extends Vue {
-}
+export default class Home extends Vue {}
 </script>
 
 <style scoped>
 .home {
-  text-align: center;
+	text-align: center;
 }
 
 .photo {
-  max-width: 95%;  
+	max-width: 95%;
 }
 
 .title-container {
-	max-width: 100%;
-	min-height: 10em;
-	justify-content: center;
-	align-items: center;
+  max-width: 100%;
+  min-height: 10em;
+  justify-content: center;
+  align-items: center;
 }
 
 .title {
-	padding: 1em;
+  padding: 1em;
 }
+
 </style>
