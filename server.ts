c78c99b93ca37745d099227147b3e674c097b949
pberge <pereberge@gmail.com>
2019-08-14 16:57:45 +0200

    heroku pushed

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
-const express = require("express");
-const port = process.env.PORT || 8080;
-const app = express();
+const express = require("express")
+const port = process.env.PORT || 8080
+const app = express()
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/dist/"))
 app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
+  res.sendfile(__dirname + "/dist/index.html")
 });
-app.listen(port);
+app.listen(port)
 
-console.log("Server started...");
+console.log("Server started...")
