293649241001900200c5d459e7560756eced7f01
pberge <pereberge@gmail.com>
2019-08-29 17:38:33 +0200

    api

--- src/frontend/shared/api.ts
+++ src/frontend/shared/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = process.env.PORT ? 'https://ainaweb.herokuapp.com/api/' : 'http://localhost:3000/api/'
+const url = ( process.env.NODE_ENV === 'development' ) ? 'http://localhost:3000/api/' : 'https://ainaweb.herokuapp.com/api/'
 
 export default {
 
